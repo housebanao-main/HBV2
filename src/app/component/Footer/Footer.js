1f8e0e15e8543b6f639918188ff08f31ce680535
housebanao-main <tech@housebanao.com>
2024-06-28 18:50:54 +0530

    Update Footer.js

--- src/app/component/Footer/Footer.js
+++ src/app/component/Footer/Footer.js
@@ -50,7 +50,7 @@ export const Footer = () => {
           </div>
 
           <div className="flex flex-col text-white lg:mr-[40px] lg:mt-[10px] mt-0 mr-[40px]">
-            <a href="mailto:invest@housebanao.com" className="text-white text-[13px]">invest@housebanao.com</a>
+            <a href="mailto:invest@housebanao.com" className="text-white text-[13px]">support@housebanao.com</a>
             <div className="w-full mt-2 text-white text-[13px]">
               <p className="w-[200px]">
                 747B, 7th floor, JMD Megapolis Sector 48, Gurgaon, Haryana 122018
