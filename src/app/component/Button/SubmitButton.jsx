21ce6d92e203f6741ab812bdcebb0d3105a40b96
housebanao-main <tech@housebanao.com>
2024-06-29 11:58:05 +0530

    Update SubmitButton.jsx

--- src/app/component/Button/SubmitButton.jsx
+++ src/app/component/Button/SubmitButton.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const SubmitButton = ({ children, className }) => {
   return (
-    <button className={`bg-[#A46254] text-white rounded-full py-[10px] px-[30px] ${className}`}>
+    <button className={`bg-[#A46254] text-white mt-8 rounded-lg py-[10px] px-[30px] ${className}`}>
       {children}
     </button>
   )
